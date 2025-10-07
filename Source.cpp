c8ff7d5b3295c38cfc452cb2fde6cceeb48109c3
sohibjonmamadjonov <sohibjonmamadjonov06@gmail.com>
2025-10-07 22:35:27 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,22 @@
 #include <vector>
-#include <utility>
 #include <iostream>
 using namespace std;
-
-template <class T>
-T swap(T a, T b) {
-	T temp = move(a);
-	a = move(b);
-	b = move(temp);
+template<typename T>
+void my_swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
 }
-
 int main() {
-	int x = 10, y = 4 ;
-    swap(x, y);
-	cout << " X = "<< x;
-	cout << " Y = " << y;
-	 
- 	
- 
-	vector<int> vec1 = { 1,2,3 };
-	vector<int> vec2 = { 4,5,6 };
+	int x = 123, y = 23;
+	swap(x, y);
+	cout << " X = " << x;
+
+	cout << " Y = " << y << endl;;
+	vector<int> vec1 = { 23,12,32 };
+
+	vector<int> vec2 = { 32,34,15 };
 	swap(vec1, vec2);
-	cout << "vec1[2] : " << vec1[2] <<endl<< "vec2[0] : " << vec2[0] << endl;
+	cout << "vec1[2] : " << vec1[2] << endl << "vec2[0] : " << vec2[0] << endl;
 	return 0;
 }
